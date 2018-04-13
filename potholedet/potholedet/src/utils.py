ac959a106f92a24f02fefda30c02eff7aad40dd8
Mohcine <mohcine.heddi@bioserenity.com>
2018-04-14 00:51:56 +0200

    add plot func

--- potholedet/potholedet/src/utils.py
+++ potholedet/potholedet/src/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.misc import imread
+import matplotlib.pyplot as plt
 
 def read_im(im_path, src='/Users/mohcine/pm/Zamohra/data/', mode='train'):
     """Reading images from em path.
@@ -40,3 +41,22 @@ def rgb2gray(im):
     r, g, b = im[:,:,0], im[:,:,1], im[:,:,2]
     gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
     return gray
+
+def plot_im(im, mode='gray'):
+    """Ploting image.
+
+    Parameters
+    ----------
+    im : np.array
+        image array.
+    mode : str
+        Default is gray.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
+    plt.imshow(im, cmap=mode)
+    plt.show()
