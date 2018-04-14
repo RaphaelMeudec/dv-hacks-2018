08ce7f17b2900b1a42e87bc2039e47b8df16a524
Mohcine <mohcine.heddi@bioserenity.com>
2018-04-15 00:50:44 +0200

    up potholedet module

---
+++ potholedet/potholedet/src/potholedet.py
@@ -0,0 +1,15 @@
+import utils as ut
+
+class PotholeDetection(object):
+    def __init__(self, im_path):
+        pass
+
+    def get_feature(self):
+        pass
+
+    def get_score(self):
+        feat = self.img_to_feature()
+        pass
+
+    def plot(self):
+        pass
