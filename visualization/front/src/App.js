cb1341cd22bd42783e402fe0c69dd72e1bab85f0
Raphael <raphael.meudec@gmail.com>
2018-04-14 19:58:29 +0200

    improve styling

--- visualization/front/src/App.js
+++ visualization/front/src/App.js
@@ -10,7 +10,9 @@ const bcgDVCoordinates = [33.9008624,-118.394406];
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isPlaying: false};
+    this.state = {
+      isPlaying: false,
+    };
 
     this.handleClick = this.handleClick.bind(this);
   }
@@ -20,6 +22,7 @@ class App extends React.Component {
       isPlaying: !prevState.isPlaying
     }))
 
+
     this.state.isPlaying ? this.refs.vidRef.pause() : this.refs.vidRef.play();
   }
 
@@ -27,28 +30,31 @@ class App extends React.Component {
     return (
       <div>
         <MainMenu />
-        <div className='leaflet-container'>
-          <Map center={bcgDVCoordinates} zoom={15}>
-            <TileLayer
-              url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-          </Map>
-        </div>
-        <div className='video-container'>
-          <video
-            width="600"
-            ref="vidRef"
-            src="https://fpdl.vimeocdn.com/vimeo-prod-skyfire-std-us/01/751/10/253758104/925178540.mp4?token=1523699231-0x446da598502696ab76abbc2eb5c927921faf22b2"
-            type="video/mp4"
-          >
-          Your browser does not support the video tag.
-          </video>
-        </div>
-        <div className='play-button'>
-          <button onClick={this.handleClick.bind(this)}>
-            {this.state.isPlaying ? 'Pause' : 'Play'}
-          </button>
+        <div className="container">
+          <div id="map">
+            <Map center={bcgDVCoordinates} zoom={15}>
+              <TileLayer
+                url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              />
+            </Map>
+          </div>
+          <div className='video-container'>
+            <video
+              width="300"
+              ref="vidRef"
+              src="https://fpdl.vimeocdn.com/vimeo-prod-skyfire-std-us/01/751/10/253758104/925178540.mp4?token=1523699231-0x446da598502696ab76abbc2eb5c927921faf22b2"
+              type="video/mp4"
+            >
+            Your browser does not support the video tag.
+            </video>
+            <h1>Hello !</h1>
+          </div>
+          <div className='play-button'>
+            <button onClick={this.handleClick.bind(this)}>
+              {this.state.isPlaying ? 'Pause' : 'Play'}
+            </button>
+          </div>
         </div>
       </div>
     );
