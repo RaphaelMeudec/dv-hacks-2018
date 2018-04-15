467b4ceb5459535e680135ac15291399300f4613
Mohcine <mohcine.heddi@bioserenity.com>
2018-04-15 02:40:41 +0200

    amend import

--- potholedet/potholedet/__main__.py
+++ potholedet/potholedet/__main__.py
@@ -1,4 +1,4 @@
-from .src import potholedet as potdet
+from src import potholedet as potdet
 
 def main(dir_path, path_score=None):
     """main function to score list of images.
@@ -21,7 +21,7 @@ def main(dir_path, path_score=None):
     if path_score is None:
         return det.get_score()
     else:
-        det.dump_score(path_score):
+        det.dump_score(path_score)
 
 if __name__ == "__main__":
     dir_path = '../../data'
