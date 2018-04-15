4a1876774d5c5eef145b0dccb6698f03ae9e8cd1
Mohcine <mohcine.heddi@bioserenity.com>
2018-04-15 02:13:15 +0200

    added main func

--- potholedet/potholedet/__main__.py
+++ potholedet/potholedet/__main__.py
@@ -1,5 +1,9 @@
+from .src import potholedet as potdet
 
-
-def predict(im_path):
-    out = 0
-    return out
+def main(im_path, path_score=None):
+    det = potdet.PotholeDetection(img_path)
+    score = det.get_score()
+    if path_score is None:
+        return det.get_score()
+    else:
+        det.dump_score(path):
