33809e563d26c7bd5bd11f3104854614f7b5e6a3
Mohcine <mohcine.heddi@bioserenity.com>
2018-04-15 02:24:01 +0200

    add docstring

--- potholedet/potholedet/__main__.py
+++ potholedet/potholedet/__main__.py
@@ -1,6 +1,21 @@
 from .src import potholedet as potdet
 
 def main(dir_path, path_score=None):
+    """main function to score list of images.
+
+    Parameters
+    ----------
+    dir_path : str
+        path to image directory.
+    path_score : str
+        if None then no dump.
+
+    Returns
+    -------
+    pd.DataFrame
+        if not path_score then return pandas DataFrame of scores otherwise ump csv file.
+
+    """
     det = potdet.PotholeDetection(dir_path)
     score = det.get_score()
     if path_score is None:
@@ -9,3 +24,5 @@ def main(dir_path, path_score=None):
         det.dump_score(path_score):
 
 if __name__ == "__main__":
+    dir_path = '../../data'
+    main(dir_path, path_score='../../data/images_score.csv')
