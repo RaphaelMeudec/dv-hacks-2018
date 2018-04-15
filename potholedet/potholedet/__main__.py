46a961ed4a57d0f15f8932624a80166da2631488
Mohcine <mohcine.heddi@bioserenity.com>
2018-04-15 02:20:20 +0200

    corrected var names

--- potholedet/potholedet/__main__.py
+++ potholedet/potholedet/__main__.py
@@ -1,9 +1,11 @@
 from .src import potholedet as potdet
 
-def main(im_path, path_score=None):
-    det = potdet.PotholeDetection(img_path)
+def main(dir_path, path_score=None):
+    det = potdet.PotholeDetection(dir_path)
     score = det.get_score()
     if path_score is None:
         return det.get_score()
     else:
-        det.dump_score(path):
+        det.dump_score(path_score):
+
+if __name__ == "__main__":
